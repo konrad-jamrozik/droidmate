8533b1596f6f3e7027c8d582cb64a4540e102c1d
Philipp Staudt <s9phstau@stud.uni-saarland.de>
2016-01-15 13:15:30 +0100

    fixed gui screens

--- dev/droidmate/projects/core/src/main/groovy/org/droidmate/exploration/strategy/ExplorationStrategy.groovy
+++ dev/droidmate/projects/core/src/main/groovy/org/droidmate/exploration/strategy/ExplorationStrategy.groovy
@@ -153,7 +153,7 @@ class ExplorationStrategy implements IExplorationStrategy
     if (lastGuiScreen == null) {
       lastGuiScreen = result.guiSnapshot.guiState
       guiStatesSeen.add(lastGuiScreen)
-      log.trace(Markers.gui,"<elements_seen>" + lastGuiScreen.widgets.size() + "</elements_seen>")
+      log.trace(Markers.gui,"<elements_seen>1</elements_seen>")
       log.trace(Markers.gui,"<gui_screens_seen>" + guiStatesSeen.size() + "</gui_screens_seen>")
     }
     //--------------
