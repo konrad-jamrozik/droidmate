636556b5c06c96c07c696ed0060ded2de58cbdb6
Konrad Jamrozik <spawarotti@jamro.pl>
2016-01-22 17:09:16 +0100

    clean up scratchpad test

---
+++ dev/droidmate/projects/core/src/test/groovy/org/droidmate/tests_misc/JavaScratchpadTestClass.java
@@ -0,0 +1,21 @@
+// Copyright (c) 2012-2016 Saarland University
+// All rights reserved.
+//
+// Author: Konrad Jamrozik, jamrozik@st.cs.uni-saarland.de
+//
+// This file is part of the "DroidMate" project.
+//
+// www.droidmate.org
+
+package org.droidmate.tests_misc;
+
+import org.junit.Test;
+
+public class JavaScratchpadTestClass
+{
+  @Test
+  public void JavaScratchpadTest()
+  {
+  }
+}
+
