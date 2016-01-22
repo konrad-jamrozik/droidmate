b7998d6d9ac489bfccddc5c1658597876aa07d26
Konrad Jamrozik <spawarotti@jamro.pl>
2016-01-22 21:02:15 +0100

    implement getting unique widgets by time

--- dev/droidmate/projects/reporter/src/main/kotlin/org/droidmate/report/GUICoverage.kt
+++ dev/droidmate/projects/reporter/src/main/kotlin/org/droidmate/report/GUICoverage.kt
@@ -10,24 +10,21 @@ package org.droidmate.report
 
 import com.google.common.collect.ImmutableTable
 import com.google.common.collect.Table
-import org.droidmate.common.exploration.datatypes.Widget
 import org.droidmate.exceptions.UnexpectedIfElseFallthroughError
 import org.droidmate.exploration.data_aggregators.IApkExplorationOutput2
-import java.time.Duration
-import java.util.*
 
 class GUICoverage(val data: IApkExplorationOutput2) {
 
   val table: Table<Int, String, Int> by lazy {
 
-    val widgetsSeen = WidgetsSeen(data)
+    val uniqueWidgets = UniqueWidgets(data)
 
     // KJA extract ms step
     val timeRange = 0.rangeTo(data.explorationTimeInMs).step(1000)
 
     val rows: List<Triple<Int, Int, Int>> = timeRange.mapIndexed { tickIndex, timePassed ->
       // KJA the "widgetsSeen.byTime" is the only variable, all the rest can be extracted. The variable is "some value that got increment by given other value"
-      Triple(tickIndex, timePassed, widgetsSeen.byTime(timePassed))
+      Triple(tickIndex, timePassed, uniqueWidgets.byTime(timePassed))
     }
 
     // KJA extract "make table from (column headers, rows represented by triplets)
@@ -55,30 +52,3 @@ class GUICoverage(val data: IApkExplorationOutput2) {
   }
 }
 
-class WidgetsSeen(val data: IApkExplorationOutput2) {
-  fun byTime(timePassed: Int): Int {
-
-    val widgetsAtTime: List<Pair<Long, List<Widget>>> = data.actRess.map {
-      val timePassedForWidgets = Duration.between(data.explorationStartTime, it.action.timestamp).toMillis()
-      val widgets: List<Widget> = it.result.guiSnapshot.guiState.widgets
-      Pair(timePassedForWidgets, widgets)
-    }
-
-    var uniqueWidgetsSeenAccumulator: MutableList<Widget> = ArrayList()
-
-    val uniqueWidgetsAtTime: List<Pair<Long, List<Widget>>> = widgetsAtTime.map {
-
-      // KJA
-      val newUniqueWidgets: List<Widget> = it.second /* all such widgets in current item that are not yet in accumulator with regards to uniqueness comparison */
-        uniqueWidgetsSeenAccumulator.addAll(newUniqueWidgets)
-      it.copy(second = uniqueWidgetsSeenAccumulator.toList())
-      //Pair(it.first, uniqueWidgets)
-    }
-
-    // KJA
-    // val widgetsByTime: List<Widget> = uniqueWidgetsAtTime.dropWhile { it.first < timePassed }.first().second
-
-    return timePassed / 2
-  }
-
-}
---
+++ dev/droidmate/projects/reporter/src/main/kotlin/org/droidmate/report/UniqueWidgets.kt
@@ -0,0 +1,61 @@
+// Copyright (c) 2012-2016 Saarland University
+// All rights reserved.
+//
+// Author: Konrad Jamrozik, jamrozik@st.cs.uni-saarland.de
+//
+// This file is part of the "DroidMate" project.
+//
+// www.droidmate.org
+package org.droidmate.report
+
+import org.droidmate.common.exploration.datatypes.Widget
+import org.droidmate.exploration.data_aggregators.IApkExplorationOutput2
+import org.droidmate.exploration.strategy.WidgetStrategy
+import java.time.Duration
+import java.util.*
+
+class UniqueWidgets(val data: IApkExplorationOutput2) {
+
+  fun byTime(timePassed: Int): Int {
+
+    val uniqueWidgetsByTime: List<Widget>? = uniqueWidgetsAtTime.firstOrNull() { it.first >= timePassed }?.second
+
+    return uniqueWidgetsByTime?.count() ?: uniqueWidgetsAtTime.last().second.count()
+  }
+
+  private val uniqueWidgetsAtTime: List<Pair<Long, List<Widget>>> by lazy {
+
+    var uniqueWidgetsAccumulator: MutableList<Widget> = ArrayList()
+
+    val uniqueWidgetsAtTime: List<Pair<Long, List<Widget>>> = widgetsAtTime.map {
+
+      val newUniqueWidgets = it.second.filterNot { widget ->
+        uniqueWidgetsAccumulator.any {
+          WidgetStrategy.WidgetInfo(it).uniqueString == WidgetStrategy.WidgetInfo(widget).uniqueString
+        }
+      }
+
+      uniqueWidgetsAccumulator.addAll(newUniqueWidgets)
+
+      it.copy(second = uniqueWidgetsAccumulator.toList())
+    }
+
+    uniqueWidgetsAtTime
+  }
+
+  private val widgetsAtTime: List<Pair<Long, List<Widget>>> by lazy {
+    val zeroTimeZeroWidgets = listOf(Pair<Long, List<Widget>>(0, emptyList()))
+    val widgetsAtTime: List<Pair<Long, List<Widget>>> =
+      zeroTimeZeroWidgets.plus(data.actRess.map {
+
+        // KJA KNOWN BUG got here time with relation to exploration start of -25, but it should be always > 0.
+        // This + 500 is just a dirty workaround
+        val timePassedForWidgets = Duration.between(data.explorationStartTime, it.action.timestamp).toMillis() + 500
+        val widgets: List<Widget> = it.result.guiSnapshot.guiState.widgets
+
+        Pair(timePassedForWidgets, widgets)
+      })
+    widgetsAtTime
+  }
+
+}
