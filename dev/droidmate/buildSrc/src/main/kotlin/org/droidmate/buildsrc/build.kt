cf35d03ac51f47f0c1c70642bd2323921d70cd84
Konrad Jamrozik <spawarotti@jamro.pl>
2016-04-15 21:34:56 +0200

    refactor

--- dev/droidmate/buildSrc/src/main/kotlin/org/droidmate/buildsrc/build.kt
+++ dev/droidmate/buildSrc/src/main/kotlin/org/droidmate/buildsrc/build.kt
@@ -19,6 +19,8 @@ import com.github.konrad_jamrozik.resolveDir
 import com.github.konrad_jamrozik.resolveRegularFile
 import java.nio.file.Paths
 
+val apks_dir = "apks"
+
 private val exeExt = if (OS.isWindows) ".exe" else ""
 
 //region Values directly based on system environment variables
@@ -38,28 +40,27 @@ val uiautomator_jar = android_platform_dir.resolveRegularFile("uiautomator.jar")
 val android_jar = android_platform_dir.resolveRegularFile("android.jar")
 //endregion
 
-val apks_dir = "apks"
-/** This string makes an assumption that the apk inliner adds the "-inlined" suffix. */
-val monitored_inlined_apk_fixture_name = "MonitoredApkFixture-debug-inlined.apk"
-
 val monitor_generator_res_name_monitor_template = "monitorTemplate.txt"
 val monitor_generator_output_relative_path = Paths.get("temp/generated_Monitor.java")
+
 val apk_inliner_param_input_default = Paths.get("input-apks")
 val apk_inliner_param_output_dir_default = Paths.get("output-apks")
 val apk_inliner_param_input = "-input"
 val apk_inliner_param_output_dir = "-outputDir"
 val AVD_dir_for_temp_files = "/data/local/tmp/"
 
+/** This string makes an assumption that the apk inliner adds the "-inlined" suffix. */
+val monitored_inlined_apk_fixture_name = "MonitoredApkFixture-debug-inlined.apk"
 /**
  * Denotes name of directory containing apk fixtures for testing. The handle to this path is expected to be obtained
  * in following ways:
- * 
- * From a build.gradle script:    
- * 
+ *
+ * From a build.gradle script:
+ *
  *   new File(sourceSets.test.resources.srcDirs[0], <this_var_reference>)
  *
  * From compiled source code:
- * 
+ *
  *   Paths.get(ClassLoader.getSystemResource(<this_var_reference>).toUri())
  */
 val apk_fixtures = "fixtures/apks"
