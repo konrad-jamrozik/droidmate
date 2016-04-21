b3b1a5992d6b22149546e3eda22eacc7fb69ddb4
Konrad Jamrozik <spawarotti@jamro.pl>
2016-04-21 02:48:20 +0200

    added main() stub for usage example and identified a problem

---
+++ dev/droidmate_usage_example/src/main/java/Main.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2012-2016 Saarland University
+// All rights reserved.
+//
+// Author: Konrad Jamrozik, jamrozik@st.cs.uni-saarland.de
+//
+// This file is part of the "DroidMate" project.
+//
+// www.droidmate.org
+import org.droidmate.frontend.DroidmateFrontend;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    // KJA current work: cannot find BuildKt from buildsrc. The problem seems to be that
+    // C:\Users\Konrad Jamrozik\.m2\repository\org\droidmate\core\dev\core-dev.jar
+    // doesn't contain BuildKt, while
+    // C:\Users\Konrad Jamrozik\.m2\repository\org\droidmate\droidmate\dev\droidmate-dev.zip
+    // contains it.
+    //
+    // This is a fundamental problem of this kind that the projects should not directly depend
+    // on buildsrc. Only gradle scripts should do it. I will have to proxy the references through
+    // resources or something. Ungh!
+    DroidmateFrontend.main("aa");
+  }
+}
