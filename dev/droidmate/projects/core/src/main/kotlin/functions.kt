7222073ac79dd469d5df6838f8f752a09b4d4243
Konrad Jamrozik <spawarotti@jamro.pl>
2016-05-07 23:42:10 +0200

    add Kotlin to 'core' project, with example fun and test.

---
+++ dev/droidmate/projects/core/src/main/kotlin/functions.kt
@@ -0,0 +1,12 @@
+// Copyright (c) 2012-2016 Saarland University
+// All rights reserved.
+//
+// Author: Konrad Jamrozik, jamrozik@st.cs.uni-saarland.de
+//
+// This file is part of the "DroidMate" project.
+//
+// www.droidmate.org
+
+fun example(param: String): String {
+  return param+"-mod"
+}
---
+++ dev/droidmate/projects/core/src/test/kotlin/FunctionsTest.kt
@@ -0,0 +1,21 @@
+// Copyright (c) 2012-2016 Saarland University
+// All rights reserved.
+//
+// Author: Konrad Jamrozik, jamrozik@st.cs.uni-saarland.de
+//
+// This file is part of the "DroidMate" project.
+//
+// www.droidmate.org
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class FunctionsTest {
+
+
+  @Test
+  fun ExampleTest() {
+
+    assertEquals("aha-mod",example("aha"))
+  }
+}
+
