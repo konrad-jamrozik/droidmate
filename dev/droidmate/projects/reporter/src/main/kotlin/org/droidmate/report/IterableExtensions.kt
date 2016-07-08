580bd00342238e20c7c371c6f462836c40985e64
Konrad Jamrozik <spawarotti@jamro.pl>
2016-07-08 16:41:13 +0200

    refactor

--- dev/droidmate/projects/reporter/src/main/kotlin/org/droidmate/report/IterableExtensions.kt
+++ dev/droidmate/projects/reporter/src/main/kotlin/org/droidmate/report/IterableExtensions.kt
@@ -9,10 +9,49 @@
 package org.droidmate.report
 
 /**
- * Map of counts of how many times given elements appears in this [Iterable].
+ * @return 
+ *   Map of counts of how many times given elements appears in this receiver [Iterable].
  */
 val <T> Iterable<T>.frequencies: Map<T, Int> get() {
   val grouped: Map<T, List<T>> = this.groupBy { it }
   val frequencies: Map<T, Int> = grouped.mapValues { it.value.size }
   return frequencies
+}
+
+/**
+ * @return 
+ *   A map from unique items to the index of first element in the receiver [Iterable] from which given unique item was
+ *   obtained. The indexing starts at 0.
+ * 
+ * @param extractItems 
+ *   A function that is applied to each element of the receiver iterable, converting it to an iterable of items.
+ * 
+ * @param extractUniqueString 
+ *   A function used to remove duplicates from all the items extracted from receiver iterable using [extractItems].
+ * 
+ */
+fun <T, TItem> Iterable<T>.uniqueItemsWithFirstOccurrenceIndex(
+  extractItems: (T) -> Iterable<TItem>,
+  extractUniqueString: (TItem) -> String
+): Map<TItem, Int> {
+
+  return this.foldIndexed(
+
+    mapOf<String, Pair<TItem, Int>>(), { index, accumulatedMap, elem ->
+
+    val uniqueStringsToItemsWithIndexes: Map<String, Pair<TItem, Int>> =
+      extractItems(elem).associate {
+        Pair(
+          extractUniqueString(it),
+          Pair(it, index + 1)
+        )
+      }
+
+    val newUniqueStrings = uniqueStringsToItemsWithIndexes.keys.subtract(accumulatedMap.keys)
+
+    val uniqueStringsToNewItemsWithIndexes = uniqueStringsToItemsWithIndexes.filterKeys { it in newUniqueStrings }
+
+    accumulatedMap.plus(uniqueStringsToNewItemsWithIndexes)
+    
+  }).map { it.value }.toMap()
 }
--- dev/droidmate/projects/reporter/src/test/kotlin/org/droidmate/report/IterableExtensionsKtTest.kt
+++ dev/droidmate/projects/reporter/src/test/kotlin/org/droidmate/report/IterableExtensionsKtTest.kt
@@ -11,8 +11,10 @@ package org.droidmate.report
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class IterableExtensionsKtTest {
+  
   @Test
   fun frequenciesTest() {
     assertThat(
@@ -30,6 +32,47 @@ class IterableExtensionsKtTest {
       )
     )
   }
+
+  @Test
+  fun uniqueItemsWithFirstOccurrenceIndexTest() {
+
+    val items = listOf(
+      listOf(
+        Pair("item1", "ignored1"),
+        Pair("ITEM1", "ignored2"),
+        Pair("item2", "ignored2"),
+        Pair("ITEM2", "ignored3"),
+        Pair("item3", "ignored4")
+      ),
+      listOf(
+        Pair("item1", "ignored1"),
+        Pair("ITEM2", "ignored1"),
+        Pair("item4", "ignored3")
+      ),
+      listOf(
+        Pair("ITEM5", "ignored1"),
+        Pair("item3", "ignored3"),
+        Pair("item4", "ignored2")
+      )
+
+    )
+    val expected = mapOf(
+      Pair("ITEM1", 1),
+      Pair("ITEM2", 1),
+      Pair("item3", 1),
+      Pair("item4", 2),
+      Pair("ITEM5", 3)
+    )
+
+    // Act
+    val actual: Map<String, Int> = items.uniqueItemsWithFirstOccurrenceIndex(
+      extractItems = { it.map { it.first } },
+      extractUniqueString = { it.toLowerCase() }
+    )
+
+    assertEquals(expected, actual)
+  }
+
 
 
 }
