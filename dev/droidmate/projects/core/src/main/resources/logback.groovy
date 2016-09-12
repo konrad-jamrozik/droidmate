5164506fcc2326011db41e5a84ee4a070e0ff6c2
Konrad Jamrozik <spawarotti@jamro.pl>
2016-09-12 16:24:50 +0200

    bugfix

--- dev/droidmate/projects/core/src/main/resources/logback.groovy
+++ dev/droidmate/projects/core/src/main/resources/logback.groovy
@@ -99,7 +99,7 @@ appender(appender_name_master, LazyFileAppender) {
   // http://logback.qos.ch/manual/filters.html#GEventEvaluator
   filter(EvaluatorFilter) {
     evaluator(GEventEvaluator) {
-      expression = "((e.loggerName.contains('$SysCmdExecutor.simpleName') || ) && (e.level == TRACE))"
+      expression = "((e.loggerName.contains('$SysCmdExecutor.simpleName')) && (e.level == TRACE))"
     }
     onMatch = DENY
     onMismatch = NEUTRAL
@@ -107,7 +107,7 @@ appender(appender_name_master, LazyFileAppender) {
   // Do not log anything from zeroturnaround, for example org.zeroturnaround.exec.stream.StreamPumper
   filter(EvaluatorFilter) {
     evaluator(GEventEvaluator) {
-      expression = "(e.loggerName.contains('zeroturnaround')"
+      expression = "(e.loggerName.contains('zeroturnaround'))"
     }
     onMatch = DENY
     onMismatch = NEUTRAL
